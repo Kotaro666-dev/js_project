1815207b91e3a31f1e6e7188398eb0751bd15e95
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-03-15 20:26:12 +0900

    added animation of keydown(pressed)

---
+++ keyboard/index.js
@@ -0,0 +1,21 @@
+// document.addEventListener("keydown", target => {
+//   let keyName = target.key;
+//   console.log(keyName);
+// });
+
+window.addEventListener("keydown", function(e) {
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+  if (!key) {
+    return;
+  }
+
+  key.classList.add("pressed");
+  const keys = document.querySelectorAll(".key");
+  keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+});
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("pressed");
+}
